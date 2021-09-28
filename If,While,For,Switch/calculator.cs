f8b784e5867bbecc153665e41e9cb592aab196a4
Selva <selvaarun22400@gmail.com>
2021-09-28 14:13:41 +0530

    Add project files.

---
+++ If,While,For,Switch/calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace If_While_For_Switch
+{
+    class calculator
+    {
+        public void Calculator()
+        {
+            int num1, num2, opt;
+            
+           Console.WriteLine("\t\tCalculator\n");
+            Console.Write("Enter the first num1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("\nEnter the second num2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nOptions:");
+            Console.WriteLine("          1.Addition");
+            Console.WriteLine("          2.Substraction");
+            Console.WriteLine("          3.Multipilication");
+            Console.WriteLine("          4.Division");
+            Console.WriteLine("          5.exit\n");
+            
+                
+            do
+            {
+                Console.Write("\nyour choice:");
+                opt = int.Parse(Console.ReadLine());
+                
+                switch (opt)
+                {
+                    case 1:
+                        Console.Write(num1 + num2);
+                        break;
+                    case 2:
+                        Console.Write(num1 - num2);
+                        break;
+                    case 3:
+                        Console.Write(num1 * num2);
+                        break;
+                    case 4:
+                        Console.Write(num1 / num2);
+                        break;
+                    case 5:
+                        break;
+                }
+
+
+            } while (opt <5);
+
+        }
+    }
+}
---
+++ If,While,For,Switch/switch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace If_While_For_Switch
+{
+    class @switch
+    {
+        public void Switch()
+        {
+            int day;
+            Console.WriteLine("Enter the number:");
+            day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
