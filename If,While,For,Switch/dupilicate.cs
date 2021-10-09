0e6318613a684b74744ba259651001d11cc9dbf4
ponindran <91453766+ponindran@users.noreply.github.com>
2021-10-09 10:06:14 +0530

    duplicate number

--- If,While,For,Switch/dupilicate.cs
+++ If,While,For,Switch/dupilicate.cs
@@ -1,11 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace If_While_For_Switch
 {
-    class dupilicate
+    class factorial
     {
-
+        public void Fact()
+        {
+            int i, fact = 1, number;
+            Console.WriteLine("enter the number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+                fact = fact * i;
+            {
+                Console.WriteLine("Factorial of " + number + "is:"+fact);
+            }
+        }
     }
 }
