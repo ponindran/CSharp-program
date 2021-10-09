c727ba182837d48aa60ba9d9c12ce2405c55fcb8
ponindran <91453766+ponindran@users.noreply.github.com>
2021-10-09 09:49:29 +0530

    Create countstring.cs

---
+++ If,While,For,Switch/countstring.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace If_While_For_Switch
+{
+    class countstring
+    {
+        public void cs()
+        {
+            String sentence = "india is my country";
+            int wordCount = 0;
+
+            for (int i = 0; i < sentence.Length - 1; i++)
+            {
+               
+                if (sentence[i] == ' ' && Char.IsLetter(sentence[i + 1]) && (i > 0))
+                {
+                    wordCount++;
+                }
+            }
+             
+            wordCount++;
+
+            
+            Console.WriteLine("Total number of words: " + wordCount);
+        }
+
+    }
+}
