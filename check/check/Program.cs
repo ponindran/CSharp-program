944f24d618a09068fecf94f78a220173a2796a74
pon indran <ponindran1212@gmail.com>
2021-10-11 10:06:56 +0530

    programs

---
+++ check/check/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace check
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[2];
+            arr[1] = 10;
+            Object o = arr;
+            int[] arr1 = (int[])o;
+            arr1[1] = 100;
+            Console.WriteLine(arr[1]);
+            ((int[])o)[1] = 1000;
+            Console.WriteLine(arr[1]);
+        }
+    }
+}
